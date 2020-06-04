2dab2fa09ed911230c48dffa6f7e868932dd46a6
SANJAY <SANJAY@HP>
2020-06-04 22:51:59 +0530

    publish restfull webservice

--- src/main/java/sks/spring/boot/rest/ws/HelloWorldSpringBootApp/HelloWorldSpringBootAppApplication.java
+++ src/main/java/sks/spring/boot/rest/ws/HelloWorldSpringBootApp/HelloWorldSpringBootAppApplication.java
@@ -2,12 +2,21 @@ package sks.spring.boot.rest.ws.HelloWorldSpringBootApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldSpringBootAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldSpringBootAppApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String sayHello() {
+		
+		return "Hello  World" ;
+	}
+	
 }
