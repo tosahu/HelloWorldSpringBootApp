e7ae7be8e714b213ae74205f4deae131849fd7a5
SANJAY <SANJAY@HP>
2020-06-08 00:24:27 +0530

    done

--- src/main/java/sks/spring/boot/rest/ws/HelloWorldSpringBootApp/HelloWorldSpringBootAppApplication.java
+++ src/main/java/sks/spring/boot/rest/ws/HelloWorldSpringBootApp/HelloWorldSpringBootAppApplication.java
@@ -2,17 +2,25 @@ package sks.spring.boot.rest.ws.HelloWorldSpringBootApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
-public class HelloWorldSpringBootAppApplication {
+@SpringBootApplication
+public class HelloWorldSpringBootAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldSpringBootAppApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelloWorldSpringBootAppApplication.class);
+	}
+	
+	
 	@GetMapping("/hello")
 	public String sayHello() {
 		
